783bb4e4fc3ba46a62d60d492735a27e420accdc
AgredaLuis <luis.agreda98@gmail.com>
2023-04-10 13:39:06 -0400

    agregando un capture para el readme.md

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import CharacterList from "./components/CharacterList";
 function App() {
   return (
     <div  className="bg-dark text-white"  >
-      <h1 className="text-center display-1 py-4"  >Rick and Morty REST API</h1>
+      <h1 className="text-center display-1 py-4" >Rick and Morty REST API</h1>
       <CharacterList />
     </div>
   );
